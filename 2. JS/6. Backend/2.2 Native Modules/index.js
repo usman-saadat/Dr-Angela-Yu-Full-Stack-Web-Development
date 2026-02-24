d6231d492a75a4f844f030ac65fc9267b00c9944
usman-saadat <usmansaadat@gmail.com>
2026-02-25 00:32:56 +0500

    Native Modules

---
+++ 2. JS/6. Backend/2.2 Native Modules/index.js
@@ -0,0 +1,12 @@
+const { clear } = require("console");
+const fs = require ("fs")
+
+// fs.writeFile("message.txt", "Hello from nodejs", (err) => {
+//     if (err) throw err;
+//     console.log('The file has been saved!');
+// });
+
+fs.readFile("./message.txt", 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
