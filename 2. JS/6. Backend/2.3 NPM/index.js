731ef281d5a5e6b2e615309560e38981dc191e01
usman-saadat <usmansaadat@gmail.com>
2026-02-26 15:14:25 +0500

    NPM

---
+++ 2. JS/6. Backend/2.3 NPM/index.js
@@ -0,0 +1,15 @@
+// import { randomSuperhero } from "superheroes";
+// const name = randomSuperhero(); 
+
+// var generateName = require('sillyname');
+
+// import generateName  from "sillyName";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import { randomSuperhero } from 'superheroes';
+
+// const name = superheroes.random();
+var superhero = randomSuperhero();
+console.log(`I am ${superhero}`);
