e92ff871e2e11f7d5933cb98a7eaeeaf9691a75c
usman-saadat <usmansaadat@gmail.com>
2026-04-02 15:05:09 +0500

    JSX Attributes and Styling

---
+++ 2. JS/14. React/5. JSX Attributes and Styling/src/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const img = "https://picsum.photos/200"
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" contentEditable= "true">My Favourite Foods</h1>
+    <div>
+      <img src={img + "?grayscale"}></img>
+    </div>
+  </div>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
