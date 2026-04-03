d1818571d70099de4b84b72a728a6b39ee406aa1
usman-saadat <usmansaadat@gmail.com>
2026-04-03 15:54:01 +0500

    Inline Styling in JSX

---
+++ 2. JS/14. React/6. Inline Styling in JSX/src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+<h1 style={{color: "red"}}>Hello World!</h1>, 
+document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+
