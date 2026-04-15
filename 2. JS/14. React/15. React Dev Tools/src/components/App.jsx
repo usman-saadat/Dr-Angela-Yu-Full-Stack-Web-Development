d6d306f1488ccbc8d24d26fefe4db6484442e477
usman-saadat <usmansaadat@gmail.com>
2026-04-15 15:04:58 +0500

    React Dev Tools

---
+++ 2. JS/14. React/15. React Dev Tools/src/components/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar 
+        img="https://i1.sndcdn.com/avatars-000108319272-vbax1b-t1080x1080.jpg"
+      />
+
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
